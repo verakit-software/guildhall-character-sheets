719e5bd3c1ed3a0a707b7a54f151869746412861
Elias Vera-Jimenez <eliasrvjimenez@gmail.com>
2026-05-31 11:54:59 -0500

    Implement Koin DI injection for the different platforms.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    init() {
+        KoinIOSKt.doInitKoinIos()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
